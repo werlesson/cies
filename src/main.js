3717139a3063b5bf1693a5d5c1f8910e5052b238
Werlesson <werlessono@gmail.com>
2019-08-30 15:07:57 -0300

    adicao do sass

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import Theme from './plugins/theme';
+
+Vue.use(Theme);
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/plugins/theme.js
@@ -0,0 +1,11 @@
+import "@/assets/sass/theme.scss";
+
+export default {
+  install(Vue) {
+    // Vue.use(GlobalComponents);
+    // Vue.use(GlobalDirectives);
+    // Vue.use(SideBar);
+    // Vue.use(Notify);
+    // Vue.use(BootstrapVue);
+  }
+}
