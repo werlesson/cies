7f358ee4d36818227fee49f5daf49672cff45eda
Werlesson <werlessono@gmail.com>
2019-09-02 11:07:07 -0300

    Adiçao da section video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <main-page />
     <schedule />
+    <video-trailer />
     <support />
   </div>
 </template>
@@ -10,12 +11,14 @@
 import MainPage from "./Home/Main";
 import Schedule from "./Home/Schedule";
 import Support from "./Home/Support";
+import VideoTrailer from "./Home/Video";
 export default {
   name: "home",
   components: {
     MainPage,
     Schedule,
-    Support
+    Support,
+    VideoTrailer
   }
 };
 </script>
---
+++ src/views/Home/Video.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="video-trailer">
+    <div class="effect-filter">
+      <a @click="toggleVideo()">
+        <img src="@/assets/img/icons/solid-right-arrow.png" alt />
+      </a>
+    </div>
+    <div v-if="showVideo" @click="toggleVideo()" class="video-frame">
+      <iframe
+        src="https://www.youtube.com/embed/edYCtaNueQY?start=10?autoplay=1"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showVideo: false
+    };
+  },
+  methods: {
+    toggleVideo() {
+      this.showVideo = !this.showVideo;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
