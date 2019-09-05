7c643ec8ff34e4747a6566ba065ce44bf134c3e0
Matheus Gurgel <matheuskillua77@gmail.com>
2019-09-05 08:56:48 -0300

    adicionada seção de feedback

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <main-page />
     <schedule />
     <video-trailer />
+    <feedback />
     <support />
   </div>
 </template>
@@ -12,13 +13,15 @@ import MainPage from "./Home/Main";
 import Schedule from "./Home/Schedule";
 import Support from "./Home/Support";
 import VideoTrailer from "./Home/Video";
+import Feedback from "./Home/Feedback";
 export default {
   name: "home",
   components: {
     MainPage,
     Schedule,
     Support,
-    VideoTrailer
+    VideoTrailer,
+    Feedback
   }
 };
 </script>
---
+++ src/views/Home/Feedback.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="newsletter-section">
+    <div class="container">
+      <h2>Encontrou Erros ou Possui Sugestões e quer Reportar para a Equipe de Desenvolvimento?</h2>
+      <h5>Envie um report para nós agora mesmo clicando no botão abaixo</h5>
+      <a href="https://forms.gle/kjnH3ffGUcMf7qeX6" class="site-btn text-black">
+        Reportar Feedback
+        <img src="@/assets/img/icons/double-arrow.png" alt="#" />
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .newsletter-section{
+    h2{
+      font-size: 30px;
+      margin-bottom: 2.5rem;
+    }
+    h5 {
+      color: white;
+      padding-bottom: 1.2rem;
+    }
+  }
+</style>
