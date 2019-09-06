459db0f5fec8d417568eacf94f766ef6b058ec3d
Werlesson <werlessono@gmail.com>
2019-09-05 23:29:22 -0300

    Imagem no informe

--- src/views/Home/Informs.vue
+++ src/views/Home/Informs.vue
@@ -1,57 +1,61 @@
 <template>
-	<section class="featured-section">
-		<!-- <div class="featured-bg set-bg" data-setbg="img/featured-bg.jpg"></div> -->
-    <div class="featured-bg set-bg">
-      <!-- <img class="" src="../../assets/img/featured-bg.jpg" alt=""  /> -->
-    </div>
-		<div class="featured-box">
-			<div class="text-box">
-				<div class="top-meta">
-          <h3>
-             Durante a fase online, como se dará a prevenção de hackers?
-          </h3>
+  <section class="featured-section">
+    <!-- <div class="featured-bg set-bg" data-setbg="img/featured-bg.jpg"></div> -->
+    <div class="featured-bg"></div>
+    <div class="featured-box">
+      <div class="text-box">
+        <div class="top-meta">
+          <h3>Durante a fase online, como se dará a prevenção de hackers?</h3>
         </div>
-				<!-- <h2 class="pb-3"> Durante a fase online, como se dará a prevenção de hackers?</h2> -->
-				<p class="">
+        <!-- <h2 class="pb-3"> Durante a fase online, como se dará a prevenção de hackers?</h2> -->
+        <p class>
           <!-- Durante a fase online, é comum o jogador ter liberdade e sim, há a possibilidade do uso de hacks durante as partidas.
           <br/>
           Quanto a isso, se um time perceber que o jogo está desequilibrado por conta de auxílios externos (hacks), ao final das partidas
           os times poderão alertar e reportar para a organização sobre o uso de auxílios por parte do outro time. Se houver a denúncia, a
           partida em questão será analisada pela organização e, se contatado o uso de auxílios, a partida será anulada e <strong>a equipe que fez uso
-          de hacks será banida</strong> da competição. -->
-          O match maker dos jogos na fase online se dará através da <a href="gamersclub.com.br">Gamers Club</a> (GC). A GC possui uma forte política contra hackers e seu louncher
+          de hacks será banida</strong> da competição.-->
+          O match maker dos jogos na fase online se dará através da
+          <a
+            href="gamersclub.com.br"
+          >Gamers Club</a> (GC). A GC possui uma forte política contra hackers e seu louncher
           contém um anti-cheat integrado que detecta e faz o banimento dos jogadores que estiverem fazendo uso de hacks.
-          <br/>
-          Ficou com dúvida quanto a performance do outro time? Ao final da partida você pode denunciar e a equipe realizadora do evento irá analisar. Constatada
-          a irregularidade a punição é o <strong>banimento</strong> da equipe beneficiada da competição e anulação da partida.
-          <br/>
-          NÃO SERÃO ADMITIDOS AUXÍLIOS EXTERNOS DE NENHUM MODO. A CIES NÃO INCENTIVA A DESPORTIVIDADE. JOGUE LIMPO!
+          <br />Ficou com dúvida quanto a performance do outro time? Ao final da partida você pode denunciar e a equipe realizadora do evento irá analisar. Constatada
+          a irregularidade a punição é o
+          <strong>banimento</strong> da equipe beneficiada da competição e anulação da partida.
+          <br />NÃO SERÃO ADMITIDOS AUXÍLIOS EXTERNOS DE NENHUM MODO. A CIES NÃO INCENTIVA A DESPORTIVIDADE. JOGUE LIMPO!
         </p>
-				<a href="#" class="read-more">Veja Mais Dúvidas Frequentes <img src="../../assets/img/icons/double-arrow.png" alt="#"/></a>
-			</div>
-		</div>
-	</section>
+        <a href="#" class="read-more">
+          Veja Mais Dúvidas Frequentes
+          <img src="../../assets/img/icons/double-arrow.png" alt="#" />
+        </a>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      
-    };
+    return {};
   },
-  methods: {
-
-  }
+  methods: {}
 };
 </script>
 
 <style lang="scss" scoped>
-.featured-box{
+.featured-box {
   padding: 50px 50px 50px 75px;
 }
 
 .text-box {
   margin: 0 !important;
-} 
+}
+
+.featured-bg {
+  background-image: url("~@/assets/img/featured-bg.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
