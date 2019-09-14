c2e0ac6db01c5fd142b3ffef9cb974908b8df2d8
Werlesson <werlessono@gmail.com>
2019-09-14 10:37:32 -0300

    Uso de variaveis no scss do componente 'SAC'

--- src/layout/Sac.vue
+++ src/layout/Sac.vue
@@ -1,10 +1,7 @@
 <template>
   <section class="sac" :class="{none: lastScrollPosition < OFFSET}">
     <div class="sac-closed" :class="{none: sac_open}" @click="sac_open = true">
-      <!-- <img src="@/assets/img/info.svg" width="10" alt /> -->
-      <p>Comentários</p>
-      <!-- <i class="far fa-comment-dots"></i> -->
-      <!-- <i class="fa fa-facebook"></i> -->
+      <p>{{text}}</p>
       <i class="fa fa-commenting-o"></i>
     </div>
     <div class="sac-open" :class="{none: !sac_open}">
@@ -12,7 +9,7 @@
         <i class="fa fa-close"></i>
       </div>
       <p class="text-center">{{title}}</p>
-      <a class="btn-register text-center" :href="linkForm">Clique aqui</a>
+      <a class="btn-feedback btn-block text-center" :href="linkForm">Clique aqui</a>
     </div>
   </section>
 </template>
@@ -24,6 +21,7 @@ export default {
       title:
         "Descreva o problema que encontrou ou compartilhe suas ideias conosco!",
       linkForm: "https://forms.gle/kjnH3ffGUcMf7qeX6",
+      text: "Feedback",
       lastScrollPosition: 0,
       OFFSET: 200,
       sac_open: false
@@ -37,11 +35,9 @@ export default {
     viewportMeta.content = "width=device-width, initial-scale=1";
     document.head.appendChild(viewportMeta);
   },
-
   beforeDestroy() {
     window.removeEventListener("scroll", this.onScroll);
   },
-
   methods: {
     onScroll() {
       if (window.pageYOffset < 0) {
@@ -59,45 +55,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.none {
-  transform: scale(0) !important;
-}
-
-.circle-close {
-  width: 30px;
-  height: 30px;
-  right: 15px;
-  top: -15px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #b71dae;
-  color: white;
-  position: absolute;
-}
+$primary-color-sac: #b71dae;
+$secondary-color-sac: #fff;
+$height-sac-open: 180px;
+$circle-close-diametro: 30px;
+$diametro-sac-closed: 50px;
+$position-mobile: 20px;
+$position-bottom-sac-open: calc(#{$height-sac-open} / 2);
+$margin-circle-close: calc(calc(#{$circle-close-diametro} * -1) / 2);
 
 .sac {
   z-index: 10;
   .sac-closed {
-    transition: all 0.3s ease;
-    cursor: pointer;
     position: fixed;
-    right: -50px;
-    bottom: calc(50% - 24px);
-    border-radius: 10px 10px 0 0;
     display: flex;
+    flex-direction: row-reverse;
     align-items: center;
     justify-content: center;
-    flex-direction: row-reverse;
-    background-color: #b71dae;
-    color: white;
-    transform: rotate(-90deg);
-    padding: 15px;
+    right: -50px;
+    bottom: calc(50% - 24px);
     width: 150px;
+    padding: 15px;
+    border-radius: 10px 10px 0 0;
+    background-color: $primary-color-sac;
+    color: $secondary-color-sac;
+    transform: rotate(-90deg);
+    transition: all 0.3s ease;
+    cursor: pointer;
     p {
       margin: 0;
-      color: white;
+      color: $secondary-color-sac;
     }
     i {
       margin-right: 10px;
@@ -106,32 +93,51 @@ export default {
   }
 
   .sac-open {
-    transition: all 0.3s ease;
     position: fixed;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    background-color: white;
-    width: 250px;
     right: 60px;
-    bottom: calc(50% - 90px);
-    height: 180px;
+    bottom: calc(50% - #{$position-bottom-sac-open});
+    width: 250px;
+    height: $height-sac-open;
     padding: 20px;
     border-radius: 10px;
+    background-color: $secondary-color-sac;
+    transition: all 0.3s ease;
   }
 }
 
-.btn-register {
-  background-color: #451855;
+.none {
+  transform: scale(0) !important;
+}
+
+.circle-close {
+  display: flex;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  right: $margin-circle-close;
+  top: $margin-circle-close;
+  width: $circle-close-diametro;
+  height: $circle-close-diametro;
+  border-radius: 50%;
+  background-color: $primary-color-sac;
+  color: white;
+  cursor: pointer;
+}
+
+.btn-feedback {
   padding: 10px 20px;
   border-radius: 7px;
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
+  background-color: $primary-color-sac;
   color: white;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
   transition: all 0.1s linear;
   &:hover {
     box-shadow: 5px 5px 5px rgba(0, 0, 0, 0);
-    transform: scale(0.95);
+    transform: scale(0.99);
   }
 }
 
@@ -141,12 +147,11 @@ export default {
       display: flex;
       align-items: center;
       justify-content: center;
-      width: 50px;
-      height: 50px;
+      bottom: $position-mobile;
+      right: $position-mobile;
+      width: $diametro-sac-closed;
+      height: $diametro-sac-closed;
       border-radius: 50%;
-      bottom: 20px;
-      padding: 5px;
-      right: 20px;
       box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.2);
       p {
         display: none;
@@ -157,8 +162,8 @@ export default {
     }
     .sac-open {
       position: fixed;
-      right: 20px;
-      bottom: 80px;
+      right: $position-mobile;
+      bottom: $position-mobile;
     }
   }
 }
