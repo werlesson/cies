21ace2b83f95c216a9d72b555c6c9bfac89e40a8
Werlesson <werlessono@gmail.com>
2019-09-14 16:52:15 -0300

    Times confirmados

--- src/views/Home/ConfirmedTeams.vue
+++ src/views/Home/ConfirmedTeams.vue
@@ -1,22 +1,37 @@
 <template>
   <section class="confirmed-teams">
-    <h1 class="team-name text-uppercase">Equipes Confirmadas</h1>
-
+    <h1 class="text-uppercase">Equipes</h1>
     <article class="teams">
-      <section class="team" v-for="(team, index) in teams" :key="index">
+      <div class="team" v-for="(team, index) in teams" :key="index">
+        <header>
+          <img :src="team.logo" :alt="`${team.name} Logo`" />
+        </header>
+        <footer>
+          <h3>{{ team.name }}</h3>
+          <div class="contact-team">
+            <a :href="`${team.steam}`">
+              <i class="fa fa-steam"></i>
+            </a>
+            <a :href="`${team.gc}`">
+              <gc class="logo-support gc" />
+            </a>
+          </div>
+        </footer>
+      </div>
+      <!-- <section class="team" v-for="(team, index) in teams" :key="index">
         <div class="footer-social-team d-flex justify-content-center pb-1">
-          <a v-if="team.steam" :href="`${team.steam}`">
+          <a :href="`${team.steam}`">
             <i class="fa fa-steam fa-2x"></i>
           </a>
-          <a v-if="team.gc" :href="`${team.gc}`">
+          <a :href="`${team.gc}`">
             <gc class="logo-support gc" />
           </a>
         </div>
-        <h1 class="name team-name">{{ team.name }}</h1>
+        <h3 class="name">{{ team.name }}</h3>
         <div class="logo">
           <img :src="team.logo" :alt="`${team.name} Logo`" />
         </div>
-      </section>
+      </section>-->
     </article>
   </section>
 </template>
@@ -71,7 +86,7 @@ export default {
           logo: require("@/assets/img/cies-logo-white.svg"),
           steam: "",
           gc: ""
-        },
+        }
       ]
     };
   }
@@ -85,110 +100,83 @@ $border: #fff;
 section.confirmed-teams {
   background-image: linear-gradient(
       180deg,
-      rgba(255, 255, 255, 0.9) 0%,
-      rgba(129, 27, 137, 0.5) 100%
+      rgba(#0c062e, 0.9) 0%,
+      rgba(#0c062e, 0.5) 100%
     ),
     url("~@/assets/img/purple-polygon.svg");
-
   background-size: contain;
   background-position: top;
-}
-
-h1 {
-  text-align: center;
-  color: $pink;
-  padding: 75px 0;
+  background-color: #0c062e;
+  padding-top: 100px;
+  h1 {
+    text-align: center;
+    color: white;
+    padding: 25px;
+  }
 }
 
 .teams {
   display: flex;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-  padding: 0 0 75px 0;
-  margin: 0 150px;
-}
-
-.teams .team {
-  border: 2px solid $border;
-  min-width: 300px;
-  height: 420px;
-  background-image: url("~@/assets/img/teams-bg.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  box-sizing: border-box;
-  display: flex;
-  align-items: flex-end;
-  justify-content: center;
-  flex-direction: column-reverse;
-  margin: 25px 100px;
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
-  &:hover {
-    background-image: linear-gradient(
-        rgba(113, 5, 134, 0.637) 0%,
-        rgba(129, 27, 137, 0.5) 100%
-      ),
-      url("~@/assets/img/teams-bg.jpg");
-    color: white;
-  }
-}
-
-.teams .team h1.name {
-  margin: 0;
-  font-size: 32px;
-  background-color: $border;
-  width: 100%;
-  padding: 10px 0;
-  text-transform: uppercase;
-}
-
-.teams .team .logo {
-  flex-grow: 1;
-  display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
-}
-
-.teams .team .logo img {
-  height: 200px;
-}
-
-.footer-social-team {
-  // padding-top: 20px;
-  // padding-bottom: 40px;
-  width: 100%;
-  background-color: #fff;
-  transition: all 0.3s ease;
-
-  a {
-    display: -ms-inline-flex;
-    display: -webkit-inline-box;
-    display: -ms-inline-flexbox;
-    display: inline-flex;
-    width: 32px;
-    height: 32px;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
+  flex-wrap: wrap;
+  padding: 2% 10%;
+  .team {
+    width: 220px;
+    height: 320px;
+    background-image: url("~@/assets/img/teams-bg.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-sizing: border-box;
+    display: flex;
     align-items: center;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
     justify-content: center;
-    border-radius: 50%;
-    color: #b01ba5;
-    font-size: 12px;
-    margin-left: 13px;
-    -webkit-transition: all 0.3s;
-    -o-transition: all 0.3s;
-    transition: all 0.3s ease;
-
+    flex-direction: column;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
+    margin: 20px 10px;
+    transition: all 0.25s ease;
     &:hover {
-      background: #b01ba5;
-      color: #fff;
+      transform: scale(1.05);
     }
-  }
-
-  a:first-child {
-    margin-left: 0;
+    header {
+      flex: 3;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      img {
+        width: 150px;
+      }
+    }
+    footer {
+      flex: 1;
+      width: 100%;
+      // background-color: rgba(white, 0.2);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      h3 {
+        text-transform: uppercase;
+        color: white;
+        font-size: 20px;
+        margin-bottom: 10px;
+      }
+      .contact-team {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        a {
+          color: white;
+          margin: 0 10px;
+          width: 35px;
+          height: 35px;
+          &:hover {
+            background-color: #0c062e;
+          }
+        }
+      }
+    }
   }
 }
 
--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -12,9 +12,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.teams{
-  padding-top: 100px;
-}
-</style>
