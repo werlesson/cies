37c741fbdff8d0e153d614453d39f37d57398e65
ivomarsan <ivomarlucio@gmail.com>
2019-09-16 13:49:25 -0300

    Transitions ???YAY or NAY???

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,21 @@
     <div id="preloder">
       <div class="loader"></div>
     </div>
+
     <header-layout />
-    <router-view />
+
+    <transition
+      name="fade"
+      mode="out-in"
+      @beforeLeave="beforeLeave"
+      @enter="enter"
+      @afterEnter="afterEnter"
+    >
+      <router-view />
+    </transition>
+
     <footer-layout />
+
     <sac />
   </div>
 </template>
@@ -20,6 +32,44 @@ export default {
     HeaderLayout,
     FooterLayout,
     Sac
+  },
+
+  data() {
+    return {
+      prevHeight: 0
+    };
+  },
+  methods: {
+    beforeLeave(element) {
+      this.prevHeight = getComputedStyle(element).height;
+    },
+    enter(element) {
+      const { height } = getComputedStyle(element);
+
+      element.style.height = this.prevHeight;
+
+      setTimeout(() => {
+        element.style.height = height;
+      });
+    },
+    afterEnter(element) {
+      element.style.height = "auto";
+    }
   }
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 200ms;
+  transition-property: height, opacity;
+  transition-timing-function: ease-in-out;
+  overflow: hidden;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
