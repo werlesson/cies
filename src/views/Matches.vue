d98353ddb68568799eb84b6246baf7081c144b59
ivomarsan <ivomarlucio@gmail.com>
2019-10-03 15:59:54 -0300

    Change btn color

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -87,22 +87,26 @@
             v-show="match.date !== 'TBA' || toConfirmShow === true"
           >
             <td class="next-logos left" :class="match.teamsId[0]">
-              <span :class="match.date === 'TBA' ? 'tba' : ''">
-                {{ match.teams[0] }}
-              </span>
+              <span :class="match.date === 'TBA' ? 'tba' : ''">{{
+                match.teams[0]
+              }}</span>
             </td>
             <td>vs</td>
             <td class="next-logos right" :class="match.teamsId[1]">
-              <span :class="match.date === 'TBA' ? 'tba' : ''">
-                {{ match.teams[1] }}
-              </span>
+              <span :class="match.date === 'TBA' ? 'tba' : ''">{{
+                match.teams[1]
+              }}</span>
             </td>
             <td>{{ match.date | moment("dddd, DD/MM HH:mm") }}</td>
             <td>Grupo {{ match.group }}</td>
           </tr>
         </tbody>
       </table>
-      <button class="btn btn-light" @click="toConfirmShow = !toConfirmShow">
+      <button
+        class="btn btn-light"
+        @click="toConfirmShow = !toConfirmShow"
+        style="color: #b01ba5"
+      >
         {{
           toConfirmShow === false
             ? "Ver confrontos a confirmar"
@@ -123,16 +127,16 @@
         >
           <div class="card">
             <p class="teams">
-              <span :class="match.scores ? 'winner' : ''">{{
-                match.teams[0]
-              }}</span>
+              <span :class="match.scores ? 'winner' : ''">
+                {{ match.teams[0] }}
+              </span>
               <span v-if="match.scores"
                 >{{ match.scores[0] }} vs {{ match.scores[1] }}</span
               >
               <span v-else>vs</span>
-              <span :class="match.scores ? 'loser' : ''">
-                {{ match.teams[1] }}
-              </span>
+              <span :class="match.scores ? 'loser' : ''">{{
+                match.teams[1]
+              }}</span>
             </p>
             <p v-if="!match.scores" style="margin: 0; text-align: center;">
               <span class="badge badge-danger">Aguardando Resultados</span>
