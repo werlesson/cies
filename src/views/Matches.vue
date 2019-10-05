c89f43aa138143dff2d12113bbd1a72be063c724
ivomarsan <ivomarlucio@gmail.com>
2019-10-05 13:13:32 -0300

    Update Calendar

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -87,15 +87,15 @@
             v-show="match.date !== 'TBA' || toConfirmShow === true"
           >
             <td class="next-logos left" :class="match.teamsId[0]">
-              <span :class="match.date === 'TBA' ? 'tba' : ''">
-                {{ match.teams[0] }}
-              </span>
+              <span :class="match.date === 'TBA' ? 'tba' : ''">{{
+                match.teams[0]
+              }}</span>
             </td>
             <td>vs</td>
             <td class="next-logos right" :class="match.teamsId[1]">
-              <span :class="match.date === 'TBA' ? 'tba' : ''">
-                {{ match.teams[1] }}
-              </span>
+              <span :class="match.date === 'TBA' ? 'tba' : ''">{{
+                match.teams[1]
+              }}</span>
             </td>
             <td>{{ match.date | moment("dddd, DD/MM HH:mm") }}</td>
             <td>Grupo {{ match.group }}</td>
@@ -127,16 +127,16 @@
         >
           <div class="card">
             <p class="teams">
-              <span :class="match.scores ? 'winner' : ''">{{
-                match.teams[0]
-              }}</span>
+              <span :class="match.scores ? 'winner' : ''">
+                {{ match.teams[0] }}
+              </span>
               <span v-if="match.scores"
                 >{{ match.scores[0] }} vs {{ match.scores[1] }}</span
               >
               <span v-else>vs</span>
-              <span :class="match.scores ? 'loser' : ''">
-                {{ match.teams[1] }}
-              </span>
+              <span :class="match.scores ? 'loser' : ''">{{
+                match.teams[1]
+              }}</span>
             </p>
             <p v-if="!match.scores" style="margin: 0; text-align: center;">
               <span class="badge badge-danger">Aguardando Resultados</span>
@@ -238,11 +238,13 @@ export default {
     });
 
     this.addMatch({
-      teams: ["app", "vac"]
+      teams: ["app", "vac"],
+      date: new Date("2019-10-07 22:00")
     });
 
     this.addMatch({
-      teams: ["txg", "kmc"]
+      teams: ["txg", "kmc"],
+      date: new Date("2019-10-05 22:30")
     });
 
     this.addMatch({
@@ -258,8 +260,7 @@ export default {
     });
 
     this.addMatch({
-      teams: ["vac", "kmc"],
-      date: new Date("2019-10-05 00:00")
+      teams: ["vac", "kmc"]
     });
 
     this.addMatch({
