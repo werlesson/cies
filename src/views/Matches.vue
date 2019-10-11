8d611f1389adf6f624ce62337b8c562002296ef8
ivomarsan <ivomarlucio@gmail.com>
2019-10-10 22:48:13 -0300

    Vac results

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -87,15 +87,15 @@
             v-show="match.date !== 'TBA' || toConfirmShow === true"
           >
             <td class="next-logos left" :class="match.teamsId[0]">
-              <span :class="match.date === 'TBA' ? 'tba' : ''">{{
-                match.teams[0]
-              }}</span>
+              <span :class="match.date === 'TBA' ? 'tba' : ''">
+                {{ match.teams[0] }}
+              </span>
             </td>
             <td>vs</td>
             <td class="next-logos right" :class="match.teamsId[1]">
-              <span :class="match.date === 'TBA' ? 'tba' : ''">{{
-                match.teams[1]
-              }}</span>
+              <span :class="match.date === 'TBA' ? 'tba' : ''">
+                {{ match.teams[1] }}
+              </span>
             </td>
             <td>{{ match.date | moment("dddd, DD/MM HH:mm") }}</td>
             <td>Grupo {{ match.group }}</td>
@@ -127,16 +127,16 @@
         >
           <div class="card">
             <p class="teams">
-              <span :class="match.scores ? 'winner' : ''">
-                {{ match.teams[0] }}
-              </span>
+              <span :class="match.scores ? 'winner' : ''">{{
+                match.teams[0]
+              }}</span>
               <span v-if="match.scores"
                 >{{ match.scores[0] }} vs {{ match.scores[1] }}</span
               >
               <span v-else>vs</span>
-              <span :class="match.scores ? 'loser' : ''">{{
-                match.teams[1]
-              }}</span>
+              <span :class="match.scores ? 'loser' : ''">
+                {{ match.teams[1] }}
+              </span>
             </p>
             <p v-if="!match.scores" style="margin: 0; text-align: center;">
               <span class="badge badge-danger">Aguardando Resultados</span>
@@ -312,8 +312,7 @@ export default {
     });
 
     this.addMatch({
-      teams: ["bbl", "pdt"],
-      date: new Date("2019-10-10 21:30")
+      teams: ["bbl", "pdt"]
     });
 
     this.addMatch({
@@ -430,12 +429,20 @@ export default {
       lobby: "https://gamersclub.com.br/lobby/partida/5960521"
     });
 
-    this.addMatch({
+    this.upMatch({
       teamsId: ["pdt", "xbg"],
       scores: [16, 9],
       map: "de_dust2",
       date: new Date("2019-10-10 20:22"),
       lobby: "https://gamersclub.com.br/lobby/partida/5964919"
+    });
+
+    this.upMatch({
+      teamsId: ["vac", "kmc"],
+      scores: [16, 4],
+      map: "de_mirage",
+      date: new Date("2019-10-10 21:59"),
+      lobby: "https://gamersclub.com.br/lobby/partida/5965660"
     });
   },
 
